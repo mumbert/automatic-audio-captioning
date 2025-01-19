a87b5f77ea2b1dceb6c6838e57a7eb0a22a23cbf
Martí Umbert <marti.umbert.@gmail.com>
2025-01-19 06:21:34 +0000

    test inference: load config file, switch to conda env

--- src/clap/test_inference.sh
+++ src/clap/test_inference.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# TO BE ABLE CONDA INSIDE THE SCRIPT
+eval "$(conda shell.bash hook)"
+
+# LOAD CONFIG AND ENV
+CONFIG_FILE="$HOME/automatic-audio-captioning/config/clap/baseline_clap.config"
+. $CONFIG_FILE # to load CONDA_ENV
+conda activate $CONDA_ENV
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+# LAUNCH
 audio_file="../../samples/freesound/784190__klankbeeld__village-bokhoven-nl-0944-am-150405_0586.wav"
 python test_inference.py $audio_file
