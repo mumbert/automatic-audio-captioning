bf466f530d314aeb6b61ab62fa2cc1bb27cb6d10
Marti Umbert <marti.umbert@gmail.com>
2025-01-21 21:14:05 +0000

    src/clap/evaluate_clap.sh: bugfix INSTALL_DIR

--- src/clap/evaluate_clap.sh
+++ src/clap/evaluate_clap.sh
@@ -5,7 +5,7 @@ export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
 # LOAD CONFIG AND ENV
 CONFIG_FILE="$HOME/automatic-audio-captioning/config/clap/baseline_clap.config"
 . $CONFIG_FILE # to load CONDA_ENV
-cd $INSTALL_DIR
+cd $INSTALL_DIR/automatic-audio-captioning
 
 # TO BE ABLE CONDA INSIDE THE SCRIPT
 eval "$(conda shell.bash hook)"
