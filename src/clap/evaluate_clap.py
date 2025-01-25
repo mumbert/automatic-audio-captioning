23cf30b9f4376e6af678ddfd8fbfef9851dae2cd
Marti Umbert <marti.umbert@gmail.com>
2025-01-25 16:30:28 +0000

    src/clap/evaluate_clap.py: include comments on top on how to generalize

--- src/clap/evaluate_clap.py
+++ src/clap/evaluate_clap.py
@@ -6,6 +6,25 @@ from msclap import CLAP
 from tqdm import tqdm
 import os
 
+##### Proposal to generalize baseline evaluation that could also be useful for trained models
+
+# Read configuration YAML file
+# config = utils.read_config()
+
+# Get Input Data
+# dataloader = utils.prepare_dataloader(config["data"])
+
+# Prepare Model to Evaluate
+# model = utils.prepare_model(config["model"])
+
+# Get Output Candidate Captions and Groundtruth
+# candidates, mult_references = model(config["predict"])
+
+# Results
+# corpus_scores = utils.evaluate(config["evaluate"])
+
+#####
+
 # params
 subset="eval" # 'dev', 'val', 'eval', 'dcase_aac_test', 'dcase_aac_analysis', 'dcase_t2a_audio', 'dcase_t2a_captions'
 batch_size=4
@@ -18,6 +37,8 @@ subsetfolder = os.path.join(datafolder, "CLOTHO_v2.1/clotho_audio_files/", subse
 
 print(f"Downloading Clotho subset: {subset} in folder {datafolder}")
 dataset = Clotho(root=datafolder, subset=subset, download=True) # dcase_aac_test
+
+print("Preparing dataloader")
 dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=BasicCollate())
 
 # Load and initialize CLAP
